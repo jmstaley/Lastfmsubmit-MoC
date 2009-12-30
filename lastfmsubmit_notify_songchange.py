d688080a40de48a40f9d9640b05ed5ea652fd4ab
jon <jon@crunchbang.(none)>
2009-12-30 16:44:11 +0000

    Initial commit

---
+++ lastfmsubmit_notify_songchange.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import subprocess
+import pynotify
+
+from optparse import OptionParser
+
+if __name__ == '__main__':
+
+    # setup parser
+    parser = OptionParser()
+
+    # set default to show notification
+    parser.set_defaults(quiet=False)
+
+    parser.add_option('-q', action='store_true', dest='quiet')
+    parser.add_option('-a', '--artist', dest='artist')
+    parser.add_option('-t', '--title', dest='title')
+    parser.add_option('-l', '--length', dest='length')
+    parser.add_option('-r', '--album', dest='album')
+
+    (options, args) = parser.parse_args()
+
+    # call lastfmsubmit
+#    subprocess.call(['/usr/lib/lastfmsubmitd/lastfmsubmit', 
+#                     '--artist', '%s' % options.artist,
+#                     '--title', '%s' % options.title, 
+#                     '--length', '%s' % options.length, 
+#                     '--album', '%s' % options.album])
+
+    # send notification to libnotify
+    if not options.quiet:
+        if pynotify.init('Notify MoC'):
+            n = pynotify.Notification('%s' % options.artist, '%s' % options.title)
+            n.set_timeout(5000)
+            n.show()
